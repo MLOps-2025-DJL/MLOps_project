5a2fe36dc4de7933fc4d0cf1bb9a6386a4b1a075
Cécile <74317560+Cecile-L@users.noreply.github.com>
2025-05-19 00:43:32 +0200

    Update test_retrain_pipeline.py

--- airflow/dags/test_retrain_pipeline.py
+++ airflow/dags/test_retrain_pipeline.py
@@ -1,22 +1,23 @@
 import pytest
 from airflow.models import DagBag
-from retrain_pipeline import *
+
+DAG_ID = "retrain_model_pipeline"
 
 @pytest.fixture(scope="module")
 def dagbag():
-    return DagBag(dag_folder="airflow/dags", include_examples=False)
+    return DagBag(dag_folder="airflow/dags", include_examples=False, read_dags_from_db=False)
 
 def test_dag_loaded(dagbag):
-    dag = dagbag.get_dag("retrain_model_pipeline")
+    dag = dagbag.dags[DAG_ID]
     assert dag is not None, "DAG 'retrain_model_pipeline' failed to load"
     assert dag.dag_id == "retrain_model_pipeline"
 
 def test_task_count(dagbag):
-    dag = dagbag.get_dag("retrain_model_pipeline")
+    dag = dagbag.dags[DAG_ID]
     assert len(dag.tasks) == 5, "Expected 5 tasks in the DAG"
 
 def test_task_ids(dagbag):
-    dag = dagbag.get_dag("retrain_model_pipeline")
+    dag = dagbag.dags[DAG_ID]
     expected_task_ids = {
         "insert_metadata_to_postgres",
         "download_images",
@@ -27,7 +28,7 @@ def test_task_ids(dagbag):
     assert set(dag.task_ids) == expected_task_ids
 
 def test_dependencies(dagbag):
-    dag = dagbag.get_dag("retrain_model_pipeline")
+    dag = dagbag.dags[DAG_ID]
 
     assert dag.get_task("insert_metadata_to_postgres").downstream_task_ids == {"download_images"}
     assert dag.get_task("download_images").downstream_task_ids == {"retrain_model"}
